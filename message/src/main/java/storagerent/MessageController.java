0c2b290023a6c6935a297c0bb3960cea97cf794d
Seokmin Jang <smjang85@gmail.com>
2021-06-21 17:05:48 +0900

    histrix 추가
    histrix 추가

--- message/src/main/java/storagerent/MessageController.java
+++ message/src/main/java/storagerent/MessageController.java
@@ -2,7 +2,19 @@ package storagerent;
 
 
 import org.springframework.web.bind.annotation.RestController;
+import storagerent.external.StorageService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MessageController {
+    private StorageService storageService;
+
+    public MessageController(StorageService storageService){
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/checkStorage")
+    String chkAndReqReserve() {
+        return storageService.checkStorage();
+    }
 }
